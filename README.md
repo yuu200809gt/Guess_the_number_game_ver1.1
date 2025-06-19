ae570999ea6b9d6b68b8c3888a51fde8177eb859
kunsei <yusei.2989gt@gmail.com>
2025-06-19 23:17:54 +0900

    firstcommit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Guess the number game（数字当てゲームver1.1）![Python](https://img.shields.io/badge/Python-3776AB?logo=python&logoColor=white)
+
+
+## 遊び方
+1. 任意の最小値〜任意の最大値の間でランダムな数字を選びます。（チャンスは5回!）
+2. 入力後に「小さすぎます」「大きすぎます」「正解!」のいずれかが表示されます。
+
+
+## ゲーム実行方法
+
+````bash
+git clone 
+cd Guess_the_number_game
+python3 main.py
+````
---
+++ main.py
@@ -0,0 +1,34 @@
+import random
+
+print("このゲームは任意の最小値と最大値を決め、5回以内に数字を当てるゲームです!")
+
+min = int(input("最小値:")) #最小値の入力
+max = int(input("最大値:")) #最大値の入力
+
+target = random.randint(min,max) #最小値と最大値の間の数字で解答を生成する
+answer_chance = 5 #試行回数5回
+
+
+for i in range(1,answer_chance+1):
+    try:
+        guess = int(input(f"[{i}回目] 数字を入力してください : "))
+        if guess < min or guess > max:
+            print("範囲内の数字を入力してください！")
+            continue
+        if guess == target:
+            print(f"正解です！{i}回目で当てました！")   
+            break
+        elif guess < target:
+            print("小さすぎます！ \n")
+        else: 
+            print("大きすぎます！")
+    except ValueError:
+        print("数字を入力してください \n")
+else:
+    print(f"残念!答えは{target}でした！")        
+
+
+
+
+
+
